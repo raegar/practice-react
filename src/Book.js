2059b336b92c4b3f9fae8b7b6b047a2c50790050
Jamie Myland <jamie.myland@peterborough.ac.uk>
2021-11-03 11:17:04 +0000

    added a named div to Book

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import './Book.css';
 
 function Book(props) {
-    return <h2>This item is a book: {props.title} by {props.author}</h2>;
+    return  <div className="Book-item">
+                <h2>This item is a book: {props.title} by {props.author}</h2>
+            </div>
   }
 
 
