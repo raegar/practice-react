c63a965eef70f62aa1a5f0b0cc5e818448f58abc
Jamie Myland <jamie.myland@peterborough.ac.uk>
2021-11-03 11:04:53 +0000

    Removed exrta semi-colon

--- src/Inventory.js
+++ src/Inventory.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Inventory(props) {
     return <>
-            <h1>This is a collection of Items</h1>;
+            <h1>This is a collection of Items</h1>
         </>
   }
 
