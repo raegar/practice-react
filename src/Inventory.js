8f934aaee5183403b056cf7a36428dc8350cf408
Jamie Myland <jamie.myland@peterborough.ac.uk>
2021-11-03 11:07:03 +0000

    Added Books to Inventory

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Book from './Book';
 
 function Inventory(props) {
     return <>
             <h1>This is a collection of Items</h1>
+            <Book title="Lord of the Rings" author="JRR Tolkien"/>
+            <Book />
         </>
   }
 
