a391f612fdaf7a1597b1bd1fda83eeaa4efcaeb2
Jamie Myland <jamie.myland@peterborough.ac.uk>
2021-11-03 10:55:30 +0000

    Added a book example to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Book from './Book';
 
 function App() {
   return (
     <div className="App">
-      
+      <Book title="Lord of the Rings" author="JRR Tolkien"/>
     </div>
   );
 }
