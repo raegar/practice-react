43afdc6777af25e9a4ff0b0bf8a8052f6ab88c7d
Jamie Myland <jamie.myland@peterborough.ac.uk>
2021-11-03 10:57:50 +0000

    Added default props to Book

--- src/Book.js
+++ src/Book.js
@@ -4,4 +4,10 @@ function Book(props) {
     return <h2>This item is a book: {props.title} by {props.author}</h2>;
   }
 
+
+  Book.defaultProps = {
+    title: "Untitled",
+    author: "Anon"
+  }
+ 
   export default Book;
